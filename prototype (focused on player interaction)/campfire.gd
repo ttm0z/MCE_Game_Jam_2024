893249af4a6231124ff36a72feefca0d4d4397a8
Dylan Winter <122752764+DylanWinter@users.noreply.github.com>
2024-01-27 13:00:04 -0330

    sprites
    animated fire, randomized trees

---
+++ prototype (focused on player interaction)/campfire.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@export var sprite: AnimatedSprite2D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	sprite.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- prototype (focused on player interaction)/tree.gd
+++ prototype (focused on player interaction)/tree.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
+@export var sprite: Sprite2D
+
 var log = preload("res://log.tscn")
+var spriteRects = [Rect2(0,0,96,96), Rect2(96, 0, 96, 96), Rect2(192, 0, 96, 96)]
 
 var treeHealth = 3
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	z_index = int(position.y/10)
-
+	spriteRects.shuffle()
+	sprite.region_rect = spriteRects[0]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
